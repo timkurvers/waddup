258df1a7cea2333a82cf84e5e74b19919288c434
Johannes Gorset <jgorset@gmail.com>
2013-10-15 23:12:26 +0200

    Refactor to use 'subject'

--- spec/waddup/cli_spec.rb
+++ spec/waddup/cli_spec.rb
@@ -1,23 +1,21 @@
 require "spec_helper"
 
 describe Waddup::CLI do
-  let(:cli) { cli = Waddup::CLI.new }
-
   context "given a text that describes sources and time" do
     before do
-      cli.run! "with mail from monday through friday"
+      subject.run! "with mail from monday through friday"
     end
 
     it "should derive sources" do
-      expect(cli.sources).to eq "mail"
+      expect(subject.sources).to eq "mail"
     end
 
     it "should derive when to start" do
-      expect(cli.from).to eq Chronic.parse "monday"
+      expect(subject.from).to eq Chronic.parse "monday"
     end
 
     it "should derive when to stop" do
-      expect(cli.to).to eq Chronic.parse "friday"
+      expect(subject.to).to eq Chronic.parse "friday"
     end
   end
 end
