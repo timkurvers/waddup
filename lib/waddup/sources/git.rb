e5b3c14ccd4e85fc6c173862860979e71983e623
Tim Kurvers <tim@moonsphere.net>
2013-10-15 23:26:29 +0200

    Extract events from Git

--- lib/waddup/sources/git.rb
+++ lib/waddup/sources/git.rb
@@ -28,6 +28,12 @@ class Waddup::Source::Git < Waddup::Source
     events.flatten!
   end
 
+  # See: https://www.kernel.org/pub/software/scm/git/docs/git-log.html#_pretty_formats
+  GIT_FORMAT = '%h %ai %s'
+
+  # Pattern to extract from the above format
+  EXTRACT_PATTERN = /([0-9a-f]{7}) ([-0-9: ]+\+\d{4}) (.+)/i
+
   # Aggregates events for given repository
   #
   # Arguments:
@@ -37,7 +43,14 @@ class Waddup::Source::Git < Waddup::Source
   #   :repo (path)
   #
   def events_for_repo(from, to, repo)
-    # `git log --author='#{author}' --since=#{from.iso8601} --until=#{to.iso8601} --format='format:%h %s %t'`
+    log = `git log --author='#{author}' --since='#{from.iso8601}' --until='#{to.iso8601}' --format='format:#{GIT_FORMAT}'`
+    log.scan(EXTRACT_PATTERN).map do |hash, datetime, subject|
+      Waddup::Event.new.tap do |e|
+        e.datetime = DateTime.parse(datetime)
+        e.subject = subject
+        e.source = self
+      end
+    end
   end
 
   # Requires Git to be installed successfully
