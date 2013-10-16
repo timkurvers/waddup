863167ab2175d7e70c26022e3d9864436923d9a9
Tim Kurvers <tim@moonsphere.net>
2013-10-16 23:56:40 +0200

    Remove superfluous spec

--- spec/waddup/event_spec.rb
+++ spec/waddup/event_spec.rb
@@ -1,14 +1,4 @@
 require 'spec_helper'
 
 describe Waddup::Event do
-
-  it 'exposes its properties' do
-    expect(subject.methods).to include *[
-      :at, :at=,
-      :until, :until=,
-      :source, :source=,
-      :subject, :subject=
-    ]
-  end
-
 end
