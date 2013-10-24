0e4d7bb05d3704e7233d06bbfffaf5abb28c9876
Tim Kurvers <tim@moonsphere.net>
2013-10-24 22:56:49 +0200

    Add preliminary stubbed specs for Apple Mail and Calendar

--- spec/waddup/sources/apple_calendar_spec.rb
+++ spec/waddup/sources/apple_calendar_spec.rb
@@ -3,9 +3,33 @@ require 'spec_helper'
 describe Waddup::Source::AppleCalendar do
 
   describe '::usable?' do
-    if described_class.osx?
-      it { should be_usable }
-    else
+    context 'when on OSX' do
+      before do
+        described_class.stub(:osx?).and_return true
+      end
+
+      context 'when AppleScript is available' do
+        before do
+          described_class.stub_shell 'osalang 2>&1', output: 'AppleScript'
+        end
+
+        it { should be_usable }
+      end
+
+      context 'when AppleScript is unavailable' do
+        before do
+          described_class.stub_shell 'osalang 2>&1', exitstatus: 1
+        end
+
+        it { should_not be_usable }
+      end
+    end
+
+    context 'when on other platforms' do
+      before do
+        described_class.stub(:osx?).and_return false
+      end
+
       it { should_not be_usable }
     end
   end
